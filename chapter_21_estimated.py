ce64020f82992605bc91b796cff4c2737f1e93c1
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2022-07-28 16:57:18 +0300

    Added chapter_21_timer.py

--- chapter_21_estimated.py
+++ chapter_21_estimated.py
@@ -1,17 +1,19 @@
 import time
+import random
+from chapter_21_timer import total, bestof, bestoftotal
 
 
 def timer(func, *args):
-    start = time.process_time()
+    start = time.time()
     for i in range(10000):
         func(*args)
-    return time.process_time() - start
+    return time.time() - start
 
 
 def gen_dct1(n1=0, n2=10):
     dct = {}
     for i in range(n1, n2):
-        dct[i] = i**2
+        dct[i] = i ** 2
     return dct
 
 
@@ -23,3 +25,27 @@ test1 = timer(gen_dct1)
 test2 = timer(dict, gen_dct2()) + timer(gen_dct2)
 
 print(test1, test2, sep='\n')
+
+randlist1 = list(range(10000))
+randlist2 = list(range(20000))
+random.shuffle(randlist2)
+randlist2 = randlist2[0:500]
+
+
+def alg(list1: list, list2: list):
+    """
+    :return:
+    """
+    res = []
+    for element2 in list2:
+        for element1 in list1:
+            if element2 == element1:
+                res.append(element2)
+    pass
+    return len(res), res
+
+
+# for i in range(10):
+#     ret = alg(randlist1, randlist2)
+#     print(alg(randlist1, randlist2), ret, sep='\n')
+print(bestoftotal(10, 10, alg, randlist1, randlist2))
---
+++ chapter_21_timer.py
@@ -0,0 +1,37 @@
+import time
+
+timer = time.time
+
+
+def total(reps, func, *pargs, **kargs):
+    """
+    Total time to run func() reps times
+    :return: (total time, last result)
+    """
+    repsrange = range(reps)
+    start = timer()
+    for i in repsrange:
+        ret = func(*pargs, **kargs)
+    elapsed = timer() - start
+    return elapsed, ret
+
+
+def bestof(reps, func, *pargs, **kargs):
+    """
+    Quickest func() among reps times
+    :return: (best time, last result)
+    """
+    best = 2 ** 32
+    for i in range(reps):
+        start = timer()
+        ret = func(*pargs, **kargs)
+        elapsed = timer() - start
+        if elapsed < best: best = elapsed
+    return best, ret
+
+
+def bestoftotal(reps1, reps2, func, *args, **kwargs):
+    """
+    Best of reps1 runs of (total of reps2 runs of func)
+    """
+    return bestof(reps1, total, reps2, func, *args, **kwargs)
